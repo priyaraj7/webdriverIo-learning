a8817b4cb90a2417340ed80b637130e30f1518bb
Priyaraj7 <suppichala@gmail.com>
2023-09-02 17:39:59 -0700

    add real time validation with webdriverio

--- test/specs/functionalScenarios.js
+++ test/specs/functionalScenarios.js
@@ -10,7 +10,7 @@ describe("Functional scenario", async () => {
     await $("=Top").click();
     await browser.pause(3000);
   });
-  it("Alert msg handler", async () => {
+  xit("Alert msg handler", async () => {
     await browser.url("https://rahulshettyacademy.com/AutomationPractice/");
     await $("#mousehover").scrollIntoView();
     await browser.pause(3000);
@@ -23,7 +23,7 @@ describe("Functional scenario", async () => {
       "https://only-testing-blog.blogspot.com/2014/09/selectable.html"
     );
     await $("button").doubleClick();
-    // not available on chrome - isAlertOpen()
+    // not available on chrome - isAlertOpen( )
     //expectChai(await browser.isAlertOpen()).to.be.true;
 
     expectChai(await browser.isAlertText()).to.be.equal(
@@ -31,5 +31,32 @@ describe("Functional scenario", async () => {
     );
     await browser.acceptAlert();
     await browser.pause(3000);
+  });
+  xit("Web Tables validation", async () => {
+    await browser.url(
+      "https://rahulshettyacademy.com/seleniumPractise/#/offers"
+    );
+
+    await $("tr th:nth-child(1)").click();
+    //await browser.pause(8000);
+    const veggiesLocator = $$("tr td:nth-child(1)");
+    let veggiesName = await veggiesLocator.map(
+      async (veg) => await veg.getText()
+    );
+    let veggiesCopy = veggiesName.slice();
+    let sortedVeggiesName = veggiesCopy.sort();
+    //let sortedVeggiesName = veggiesName.sort();
+    await expectChai(veggiesName).to.eql(sortedVeggiesName);
+  });
+  it("Search for an item", async () => {
+    await browser.url(
+      "https://rahulshettyacademy.com/seleniumPractise/#/offers"
+    );
+    await $("#search-field").setValue("apple");
+    const firstElementLocator = await $$(
+      "tbody tr:nth-child(1) td:first-child"
+    );
+    const veggies = await firstElementLocator[0].getText();
+    await expectChai(veggies).to.match(/apple/);
   });
 });
